9465362422006741cbc287836f3c492f0a786d03
HiraiKyo <kyo.mikhael@gmail.com>
2024-02-15 11:38:17 +0900

    検証用Subscriber作成!

---
+++ src/capc/scripts/listener.py
@@ -0,0 +1,14 @@
+import rospy
+from std_msgs.msg import Float32 as Float
+
+def callback(data):
+  rospy.loginfo(rospy.get_caller_id() + "inner_temp=%f", data.data)
+  
+def listener():
+  rospy.init_node('listener', anonymous=True)
+  rospy.Subscriber('/watchdog/inner_temp', Float, callback)
+  
+  rospy.spin()
+  
+if __name__ == '__main__':
+  listener()
